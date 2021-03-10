52303b45b8eb29d18b4ec542e9830f9af816d81d
mul53 <mulengabowa53@gmail.com>
2021-03-09 23:33:08 -0800

    added swap event and changes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,40 @@
-import { BigDecimal } from '@graphprotocol/graph-ts'
-import { Transfer } from '../generated/ERC20/ERC20'
-import { User } from '../generated/schema'
+import { BigDecimal, log } from "@graphprotocol/graph-ts";
+import { Transfer } from "../generated/ERC20/ERC20";
+import { Swap } from "../generated/Pair/Pair";
+import { User } from "../generated/schema";
 
-let lpTokenAdress = '0x470BC451810B312BBb1256f96B0895D95eA659B1'
+let lpTokenAdress = "0x470bc451810b312bbb1256f96b0895d95ea659b1";
 
 export function handleTransfer(event: Transfer): void {
-  if (event.params.to.toHexString() !== lpTokenAdress) return
+    if (event.params.to.toHexString() == lpTokenAdress) {
+        let id = event.params.from.toHexString()
+		
+		let user = User.load(id)
+        if (user == null) {
+            user = new User(id)
+            user.totalVolume = BigDecimal.fromString("0")
+        }
 
-  let id = event.params.from.toHexString()
-  let user = User.load(id)
-  if (user == null) {
-    user = new User(id)
-    user.totalVolume = BigDecimal.fromString('0')
-  }
+        user.totalVolume = user.totalVolume.plus(
+            event.params.value.toBigDecimal()
+        )
 
-  user.totalVolume = user.totalVolume.plus(event.params.value.toBigDecimal())
+        user.save()
+    }
+}
 
-  user.save()
+export function handleSwap(event: Swap): void {
+    let id = event.params.to.toHexString()
+
+    let user = User.load(id)
+    if (user == null) {
+        user = new User(id)
+        user.totalVolume = BigDecimal.fromString("0")
+    }
+
+    user.totalVolume = user.totalVolume
+        .plus(event.params.amount0In.toBigDecimal())
+		.plus(event.params.amount0Out.toBigDecimal())
+		
+	user.save()
 }
